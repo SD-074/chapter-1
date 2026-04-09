3ed30d97cc76b5a3266d5f78d5fb9c75ec777ae6
KhaledAlabssi <khaled.absi5@gmail.com>
2026-04-09 15:33:07 +0200

    add arrays session code

---
+++ arrays/excercise.js
@@ -0,0 +1,24 @@
+// Join the army app:
+
+// logic: man > 18 & < 40; woman > 20 & < 35;
+
+// write a function getUserInfo that propmpt the user for (name, age, gender)
+// Store the user input in array
+// write another function canJoin that to check if the user can join the army or not
+// wirte a third function notifyUser to notify the user if they can join the army or not
+
+let userInfoArray = []
+function getUserInfo (){
+
+
+let userName = prompt("Welcome to x army program");
+}
+
+function canJoin (array) {
+
+}
+
+function notifyUser (boolean) {
+
+}
+
---
+++ arrays/main.js
@@ -0,0 +1,47 @@
+// arrays
+// length and index
+// push, pop, shift, unshift
+// loops for array
+
+let arr = [];
+const name = "Sam"
+console.log(arr);
+
+const pushReturn = arr.push("Car")
+
+
+const pushReturn2 = arr.push("Wall")
+const pushReturn3 = arr.push("Book")
+console.log(pushReturn);
+console.log(pushReturn2);
+console.log(pushReturn3);
+
+console.log(arr);
+
+
+
+arr.pop()
+
+console.log(arr);
+
+
+
+// delete the last item been added
+
+
+// delete the first item 
+
+
+// add item to the start of the array
+
+
+// log your array at each step above
+
+
+
+
+
+
+
+
+
