ed2b9945756a3e536105b2a31ed1d706fa5924c0
KhaledAlabssi <khaled.absi5@gmail.com>
2026-04-09 16:07:40 +0200

    update array code

--- arrays/main.js
+++ arrays/main.js
@@ -1,47 +1,81 @@
-// arrays
-// length and index
-// push, pop, shift, unshift
-// loops for array
+// // arrays
+// // length and index
+// // push, pop, shift, unshift
+// // loops for array
 
-let arr = [];
-const name = "Sam"
+// let arr = [];
+// const name = "Sam"
+// console.log(arr);
+
+// const pushReturn = arr.push("Car")
+
+
+// const pushReturn2 = arr.push("Wall")
+// const pushReturn3 = arr.push("Book")
+// console.log(pushReturn);
+// console.log(pushReturn2);
+// console.log(pushReturn3);
+
+// console.log(arr);
+
+
+
+// arr.pop()
+
+// console.log(arr);
+
+// let empArray = ["Wall", "War", "Book"]
+// empArray.push()
+// // let popResult = empArray.pop()
+// // console.log(popResult);
+
+// // let shiftOnEmp = empArray.shift()
+// // console.log(shiftOnEmp);
+
+// empArray.unshift("cat", "car", "case")
+
+// console.log(empArray);
+
+// // delete the last item been added
+
+
+// // delete the first item 
+
+
+// // add item to the start of the array
+
+
+// // log your array at each step above
+
+
+
+
+
+
+
+
+
+let arr = ["Wall", "Fun", "Book"]
+
+arr[0]
+arr[1]
+arr[2]
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    // search for Fun
+    if(element == "Fun"){
+
+        console.log("We have fun");
+        break;
+        
+
+    }
+  
+    
+
+    
+}
+
 console.log(arr);
-
-const pushReturn = arr.push("Car")
-
-
-const pushReturn2 = arr.push("Wall")
-const pushReturn3 = arr.push("Book")
-console.log(pushReturn);
-console.log(pushReturn2);
-console.log(pushReturn3);
-
-console.log(arr);
-
-
-
-arr.pop()
-
-console.log(arr);
-
-
-
-// delete the last item been added
-
-
-// delete the first item 
-
-
-// add item to the start of the array
-
-
-// log your array at each step above
-
-
-
-
-
-
-
-
 
